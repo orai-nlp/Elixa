5c991fcf323402effb01b987f090e19c635273c5
Iñaki San Vicente <i.sanvicente@elhuyar.eus>
2017-06-21 11:21:28 +0200

    updated test_Elixa.sh

--- test_Elixa.sh
+++ test_Elixa.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
-java -jar target/elixa-0.9.2.jar tag-gp -f tabNotagged -p elixa-models-0.9/en-twt.cfg -l en -m elixa-models-0.9/en-twt.model -cn 3 < src/main/resources/examples/example_en.tsv 
-java -jar target/elixa-0.9.2.jar tag-gp -f tabNotagged -p elixa-models-0.9/fr-twt.cfg -l fr -m elixa-models-0.9/fr-twt.model -cn 3 < src/main/resources/examples/example_fr.tsv 
-java -jar target/elixa-0.9.2.jar tag-gp -f tabNotagged -p elixa-models-0.9/eu-twt.cfg -l eu -m elixa-models-0.9/eu-twt.model -cn 3 < src/main/resources/examples/example_eu.tsv 
-java -jar target/elixa-0.9.2.jar tag-gp -f tabNotagged -p elixa-models-0.9/es-twt.cfg -l es -m elixa-models-0.9/es-twt.model -cn 3 < src/main/resources/examples/example_es.tsv 
+
+# the folowing commented commands show how to use especific models rather than those built-in
+#java -jar target/elixa-0.9.2.jar tag-gp -f tabNotagged -p elixa-models-0.9/en-twt.cfg -l en -m elixa-models-0.9/en-twt.model -cn 3 < src/main/resources/examples/example_en.tsv 
+#java -jar target/elixa-0.9.2.jar tag-gp -f tabNotagged -p elixa-models-0.9/fr-twt.cfg -l fr -m elixa-models-0.9/fr-twt.model -cn 3 < src/main/resources/examples/example_fr.tsv 
+#java -jar target/elixa-0.9.2.jar tag-gp -f tabNotagged -p elixa-models-0.9/eu-twt.cfg -l eu -m elixa-models-0.9/eu-twt.model -cn 3 < src/main/resources/examples/example_eu.tsv 
+#java -jar target/elixa-0.9.2.jar tag-gp -f tabNotagged -p elixa-models-0.9/es-twt.cfg -l es -m elixa-models-0.9/es-twt.model -cn 3 < src/main/resources/examples/example_es.tsv 
+
+# polarity tagging with built-in models
+java -jar target/elixa-0.9.2.jar tag-gp -f tabNotagged -cn 3 -l en < src/main/resources/examples/example_en.tsv 
+java -jar target/elixa-0.9.2.jar tag-gp -f tabNotagged -cn 3 -l fr < src/main/resources/examples/example_fr.tsv 
+java -jar target/elixa-0.9.2.jar tag-gp -f tabNotagged -cn 3 -l eu < src/main/resources/examples/example_eu.tsv 
+java -jar target/elixa-0.9.2.jar tag-gp -f tabNotagged -cn 3 -l es < src/main/resources/examples/example_es.tsv 
 
